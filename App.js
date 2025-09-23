3c3a33e5cc491cdcdba209651fc99332de65a2ee
Sumit <Sumitmalpure1089@gmail.com>
2025-09-23 21:22:33 +0530

    Rabbitti Food Deliver App

--- App.js
+++ App.js
@@ -2,7 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
+const HeaderComponent = () => {
+return (<div className="header">
+    <div className="logo">  
+        <img src="https://images-platform.99static.com//XjwbkQ5tf1RpCZSNedwjAB8DyTE=/0x0:1000x1000/fit-in/590x590/99designs-contests-attachments/95/95490/attachment_95490984" alt="logo" />
+    </div>
+    <div className="nav-items">
+        <ul>
+            <li>Home</li>   
+            <li>About</li>
+            <li>Contact</li>
+            <li>Cart</li>
+        </ul>
+    </div>
+</div>);
+
+}
+
 //React Element => Object => HTMLElement (Rendering on DOM)
-const heading = React.createElement("h1", { id: "heading" }, "Hello World from React ");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(<HeaderComponent/>);
